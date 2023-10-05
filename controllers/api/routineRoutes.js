03464890fb4a5ae5c32733e1b1e4188c154f181f
wayne80361 <wayne80361@gmail.com>
2023-10-05 10:45:03 -0400

    add GET ALL for routine route

--- controllers/api/routineRoutes.js
+++ controllers/api/routineRoutes.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
+const Routine = require("../../models/Routine.js");
+
+// GET ALL request
+// api/routines
+router.get("/", async (req, res) => {
+  try {
+    const routineData = await Routine.findAll({});
+
+    const routines = routineData.map((routine) => routine.get({ plain: true }));
+    // ***** May need to swap out the render page ******
+    res.render("routine", {
+      routines,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
