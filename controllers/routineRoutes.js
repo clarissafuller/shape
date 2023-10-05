e13bcf4725d02075657a33dc31f39fa5043bacf6
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-05 15:34:41 -0400

    adds render routes for routines

---
+++ controllers/routineRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { Routine, Workout } = require('../models');
+
+// GET all routines for make-routine page
+router.get('/', async (req, res) => {
+  try {
+    const dbRoutineData = await Routine.findAll();
+
+    const routines = dbRoutineData.map((routine =>
+      routine.get({ plain: true })
+    );
+
+    res.render('make-routine', {
+      routines,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one routine
+router.get('/routine/:id', async (req, res) => {
+  try {
+    const dbRoutineData = await Routine.findByPk(req.params.id, {
+      include: [
+        {
+          model: Workout,
+          attributes: [
+            'name',
+            'muscle',
+            'equipment',
+            'instructions',
+          ],
+        },
+      ],
+    });
+
+    const routine = dbRoutineData.get({ plain: true });
+    res.render('make-routine', { routine });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
