db470a84914e62c27dc67772939639c3592f4f99
Taylor <taysully94@gmail.com>
2023-10-05 21:40:34 -0400

    adds function to possibly render the results in the sidebar, commented out for the push

--- public/js/make-routine.js
+++ public/js/make-routine.js
@@ -3,29 +3,51 @@ const routineButtons = document.querySelectorAll(".add-routine");
 const setsInput = document.getElementById("sets-input");
 const exerciseId = document.getElementById("exerciseId");
 
+//DATA
 const addedExercises = [];
 
-//DATA
+
 
 //FUNCTIONS
 const formSubmitHandler = function (event) {
   event.preventDefault();
 
-  console.log(event.target.parentNode.children[0].children[1].value);
-  //get name
-
+  //testing how to get data from input
+//   console.log(event.target.parentNode.children[0].children[1].value);
+  
+//target and trim input data 
   const weight = event.target.parentNode.children[0].children[1].value.trim();
   const sets = event.target.parentNode.children[1].children[1].value.trim();
 
+  //get exercise ID and name 
   const exerciseId = event.target.parentNode.children[1].children[2].value;
   const name = event.target.parentNode.children[1].children[3].value;
+
   //add to array of added exercises
   addedExercises.push({ weight, sets, exerciseId, name });
-
-  //make function for rendering exercises to the "routine"
   console.log(addedExercises);
+
+
+//function to render added exercise to sidebar
+// const addToRoutine = (addedExercises) => {
+
+// addedExercises.forEach(function (exercises) {
+
+// const exerciseName = exercises.name
+// const exerciseWeight = exercises.weight
+// const exerciseSets = exercises.sets
+
+// const cardContent = `<div class="card-body">
+// <h3 class="card-title text-center">${exerciseName}</h3>
+// <p text-center>${exerciseWeight}, ${exerciseSets}</p>
+// </div>`
+// //meh????
+// })
+    
 };
 
+
+  
 //USER INTERACTIONS
 
 //INITIALIZATIONS
