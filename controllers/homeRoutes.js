9c35042bf29716295051161d580edf3c609cf695
wayne80361 <wayne80361@gmail.com>
2023-10-09 20:17:47 -0400

    fix some link bugs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -105,7 +105,11 @@ router.get("/make-routine/:id", async (req, res) => {
       exercise.get({ plain: true })
     );
     const routine = dbRoutineData.get({ plain: true });
-    res.render("make-routine", { routine, exercises });
+    res.render("make-routine", {
+      routine,
+      exercises,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
