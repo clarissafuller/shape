c2541482afdd8210c34be0844a91049afbcbf8c5
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-09 22:33:13 -0400

    adds functioning add routine button and adds routine to database

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const exerciseRoutes = require("./exerciseRoutes.js");
 const routineRoutes = require("./routineRoutes.js");
+const routineExerciseRoutes = require("./routineExerciseRoutes.js");
 
 router.use("/users", userRoutes);
 router.use("/exercises", exerciseRoutes);
 router.use("/routines", routineRoutes);
+router.use("/routineExercises", routineExerciseRoutes);
 
 module.exports = router;
